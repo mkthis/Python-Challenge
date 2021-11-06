c42764974f89c2b54a71476841801f8035e22f45
Mary <marykmcbride@gmail.com>
2021-11-06 16:21:34 -0500

    Final Python Edits

--- PyBank/main.py
+++ PyBank/main.py
@@ -21,18 +21,40 @@ with open(csvpath, 'r') as csvfile:
     Total = sum(Profits)
 
     print(f'Total: ${Total}')
-    
-    change = round((Profits[-1] - Profits[0])/len(Profits), 2)
-    print(f"Average Change: ${change}")
 
-    maximum = max(Profits)
-    indexing = Profits.index(maximum)
-    increasepr = Dates[indexing]
-    print(f'Greatest Increase in Profits: {increasepr} (${maximum})')
+    change = []
+    for i in range(len(Profits)-1):
+        change.append(Profits[i +1]- Profits[i])
 
-    minimum = min(Profits)
-    indexing = Profits.index(minimum)
-    increasepr = Dates[indexing]
-    print(f'Greatest Decrease in Profits: {increasepr} (${minimum})')
+    print(f"Average Change: $ {round(sum(change)/len(change), 2)}")
+
+
+    maximum = max(change)
+    indexing = change.index(maximum)
+    increasepr = Dates[indexing + 1]
+    print(f'Greatest Increase in Change: {increasepr} (${maximum})')
+
+    minimum = min(change)
+    indexing2 = change.index(minimum)
+    increasepr2 = Dates[indexing2 + 1]
+    print(f'Greatest Decrease in Change: {increasepr} (${minimum})')
+
+    output_path = os.path.join("Analysis", "Financial_Analysis.txt")
+    f = open(output_path, "w")
+    f.write("Financial Analysis")
+    f.write("\n")
+    f.write("-----------------------------------")
+    f.write("\n")
+    f.write((f"Total Months: {num_months}"))
+    f.write("\n")
+    f.write((f"Total Profits: ${sum(Profits)}"))
+    f.write("\n")
+    f.write((f"Average Change in Profits: ${round(sum(change)/len(change), 2)}"))
+    f.write("\n")
+    f.write((f"Greatest Increase in Profits: {increasepr}, (${maximum})"))
+    f.write("\n")
+    f.write((f"Greatest Decrease in Profits: {increasepr2}, (${minimum})"))
+    f.write("\n")
+    f.close
 
      
